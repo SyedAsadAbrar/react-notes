41ed92a874bf7007614742088f5021c23b216a77
Syed Asad Abrar <syed.abrar@careem.com>
2022-10-10 01:53:58 +0500

    Kind of complete

--- src/components/notes-container/index.js
+++ src/components/notes-container/index.js
@@ -20,15 +20,10 @@ import NoteDTO from "./note/NoteDTO";
 import {actionTypes} from "../../constants/components/notes-container";
 import {TransitionGroup} from "react-transition-group";
 import {loremIpsum} from "lorem-ipsum";
-import {getRandomNum, getCurrentDateAndTime} from "../../utils/common";
+import {getCurrentDateAndTime, getRandomNum} from "../../utils/common";
 import {SentimentDissatisfied} from "@mui/icons-material";
 
-const notesMock = [
-    new NoteDTO("First Note", getCurrentDateAndTime(), true, loremIpsum({count: getRandomNum(1, 5)})),
-    new NoteDTO("Second Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)})),
-    new NoteDTO("Third Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)})),
-    new NoteDTO("Fourth Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)}))
-];
+const notesMock = [new NoteDTO("First Note", getCurrentDateAndTime(), true, loremIpsum({count: getRandomNum(1, 5)})), new NoteDTO("Second Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)})), new NoteDTO("Third Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)})), new NoteDTO("Fourth Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)}))];
 
 const SlideTransition = (props) => <Slide {...props} direction="up"/>;
 
@@ -71,44 +66,46 @@ const NotesContainer = () => {
         setNotesInputFocused(false)
     };
 
-    return (
-        <Box sx={{mt: 2}}>
+    return (<Box sx={{mt: 2}}>
             <ClickAwayListener onClickAway={clickAwayHandler}>
                 <Box>
-                    {(isNotesInputFocused) &&
-                        <TextField fullWidth label="Title" id="noteTitle" variant={"filled"}
-                                   value={newNoteTitle}
-                                   onChange={({target: {value}}) => setNewNoteTitle(value)}
-                                   sx={{borderBottomLeftRadius: 0, borderBottomRightRadius: 0, borderBottom: "none"}}
-                        />
-                    }
+                    {(isNotesInputFocused) && <TransitionGroup>
+                        <Collapse key={"newNoteTitleCollapse"}>
+                            <TextField fullWidth label="Title" id="noteTitle"
+                                       variant={"filled"}
+                                       value={newNoteTitle}
+                                       onChange={({target: {value}}) => setNewNoteTitle(value)}
+                                       autoComplete={"off"}
+
+                            />
+                        </Collapse>
+                    </TransitionGroup>}
                     <TextField fullWidth label="Take a note..." id="noteDetails" variant={"filled"}
                                onFocus={() => setNotesInputFocused(true)}
                                onChange={({target: {value}}) => setNewNote(value)}
                                value={newNote}
+                               autoComplete={"off"}
                                InputProps={{
-                                   endAdornment: isNotesInputFocused ?
-                                       <InputAdornment position="end">
-                                           <Button variant="text" color="primary"
-                                                   onClick={() => editNotes(null, actionTypes.ADD, {
-                                                       title: newNoteTitle,
-                                                       note: newNote
-                                                   })}>Save</Button>
-                                           <Button variant="text" color="warning"
-                                                   onClick={clickAwayHandler}>Discard</Button>
-                                       </InputAdornment> : null,
+                                   endAdornment: isNotesInputFocused ? <InputAdornment position="end">
+                                       <Button variant="text" color="primary"
+                                               onClick={() => editNotes(null, actionTypes.ADD, {
+                                                   title: newNoteTitle, note: newNote
+                                               })}>Save</Button>
+                                       <Button variant="text" color="warning"
+                                               onClick={clickAwayHandler}>Discard</Button>
+                                   </InputAdornment> : null,
                                }}/>
                 </Box>
             </ClickAwayListener>
             <List>
-                <TransitionGroup>{notes.map((note, index) =>
-                    <Collapse key={note.title}>
+                <TransitionGroup>
+                    {notes.map((note, index) => <Collapse key={note.title}>
                         <Grid item xs={6}>
                             <Note title={note.title} date={note.date} isFavorited={note.isFavorited}
-                                  details={note.details} changeNoteFunc={(actionType) => editNotes(index, actionType)}/>
+                                  details={note.details}
+                                  changeNoteFunc={(actionType) => editNotes(index, actionType)}/>
                         </Grid>
-                    </Collapse>
-                )}
+                    </Collapse>)}
                 </TransitionGroup>
             </List>
             <Snackbar open={isSnackBarOpen} autoHideDuration={3000} onClose={handleSnackbarClose}
@@ -117,15 +114,12 @@ const NotesContainer = () => {
                     Note shared successfully!
                 </Alert>
             </Snackbar>
-            {!notes.length &&
-                <Container sx={{textAlign: "center"}}>
-                    <SentimentDissatisfied fontSize={"large"} color={"disabled"}/>
-                    <Typography variant="subtitle2" sx={{color: "text.disabled"}}>It's a bit empty in here... Why not
-                        add a new note?</Typography>
-                </Container>
-            }
-        </Box>
-    );
+            {!notes.length && <Container sx={{textAlign: "center"}}>
+                <SentimentDissatisfied fontSize={"large"} color={"disabled"}/>
+                <Typography variant="subtitle2" sx={{color: "text.disabled"}}>It's a bit empty in here... Why not
+                    add a new note?</Typography>
+            </Container>}
+        </Box>);
 }
 
 export default NotesContainer;
