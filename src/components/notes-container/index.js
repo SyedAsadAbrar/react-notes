7e01fb7a4da58cad208b05f1b5e3a02fb6925225
Syed Asad Abrar <syed.abrar@careem.com>
2022-10-10 01:49:16 +0500

    final

--- src/components/notes-container/index.js
+++ src/components/notes-container/index.js
@@ -1,28 +1,33 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import Note from "./note";
-import {Alert, Box, Collapse, Container, Grid, List, Slide, Snackbar} from "@mui/material";
+import {
+    Alert,
+    Box,
+    Button,
+    ClickAwayListener,
+    Collapse,
+    Container,
+    Grid,
+    InputAdornment,
+    List,
+    Slide,
+    Snackbar,
+    TextField,
+    Typography
+} from "@mui/material";
 import NoteDTO from "./note/NoteDTO";
-import {useState} from "react";
 import {actionTypes} from "../../constants/components/notes-container";
 import {TransitionGroup} from "react-transition-group";
 import {loremIpsum} from "lorem-ipsum";
-import {getRandomNum} from "../../constants/common";
+import {getRandomNum, getCurrentDateAndTime} from "../../utils/common";
 import {SentimentDissatisfied} from "@mui/icons-material";
-import Typography from "@mui/material/Typography";
-
-const todaysDate = new Date().toLocaleDateString('en-pk', {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric"
-});
 
 const notesMock = [
-    new NoteDTO("First Note", todaysDate, true, loremIpsum({count: getRandomNum(1, 5)})),
-    new NoteDTO("Second Note", todaysDate, false, loremIpsum({count: getRandomNum(1, 5)})),
-    new NoteDTO("Third Note", todaysDate, false, loremIpsum({count: getRandomNum(1, 5)})),
-    new NoteDTO("Fourth Note", todaysDate, false, loremIpsum({count: getRandomNum(1, 5)}))
+    new NoteDTO("First Note", getCurrentDateAndTime(), true, loremIpsum({count: getRandomNum(1, 5)})),
+    new NoteDTO("Second Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)})),
+    new NoteDTO("Third Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)})),
+    new NoteDTO("Fourth Note", getCurrentDateAndTime(), false, loremIpsum({count: getRandomNum(1, 5)}))
 ];
 
 const SlideTransition = (props) => <Slide {...props} direction="up"/>;
@@ -30,8 +35,12 @@ const SlideTransition = (props) => <Slide {...props} direction="up"/>;
 const NotesContainer = () => {
     const [notes, setNotes] = useState(notesMock);
     const [isSnackBarOpen, setSnackBarOpen] = useState(false);
+    const [isNotesInputFocused, setNotesInputFocused] = useState(false);
 
-    const editNotes = (index, actionType) => {
+    const [newNoteTitle, setNewNoteTitle] = useState("");
+    const [newNote, setNewNote] = useState("");
+
+    const editNotes = (index, actionType, detailObj = false) => {
         const oldNotes = [...notes];
 
         console.log("editNotes called", index, actionType)
@@ -40,8 +49,11 @@ const NotesContainer = () => {
             setNotes(oldNotes.filter((note, noteIndex) => noteIndex !== index));
         } else if (actionType === actionTypes.TOGGLE_FAVORITE) {
             setNotes(oldNotes.map((note, noteIndex) => new NoteDTO(note.title, note.date, noteIndex === index ? !note.isFavorited : note.isFavorited, note.details)));
-        } else {
+        } else if (actionType === actionTypes.SHARE) {
             setSnackBarOpen(true);
+        } else {
+            setNotes([new NoteDTO(detailObj.title, getCurrentDateAndTime(), false, detailObj.note), ...notes]);
+            clickAwayHandler();
         }
     };
 
@@ -49,15 +61,53 @@ const NotesContainer = () => {
         setSnackBarOpen(false);
     }
 
+    useEffect(() => {
+        console.log("isNotesInputFocused changed", isNotesInputFocused)
+    }, [isNotesInputFocused]);
+
+    const clickAwayHandler = () => {
+        setNewNoteTitle("");
+        setNewNote("");
+        setNotesInputFocused(false)
+    };
+
     return (
         <Box sx={{mt: 2}}>
-
+            <ClickAwayListener onClickAway={clickAwayHandler}>
+                <Box>
+                    {(isNotesInputFocused) &&
+                        <TextField fullWidth label="Title" id="noteTitle" variant={"filled"}
+                                   value={newNoteTitle}
+                                   onChange={({target: {value}}) => setNewNoteTitle(value)}
+                                   sx={{borderBottomLeftRadius: 0, borderBottomRightRadius: 0, borderBottom: "none"}}
+                        />
+                    }
+                    <TextField fullWidth label="Take a note..." id="noteDetails" variant={"filled"}
+                               onFocus={() => setNotesInputFocused(true)}
+                               onChange={({target: {value}}) => setNewNote(value)}
+                               value={newNote}
+                               InputProps={{
+                                   endAdornment: isNotesInputFocused ?
+                                       <InputAdornment position="end">
+                                           <Button variant="text" color="primary"
+                                                   onClick={() => editNotes(null, actionTypes.ADD, {
+                                                       title: newNoteTitle,
+                                                       note: newNote
+                                                   })}>Save</Button>
+                                           <Button variant="text" color="warning"
+                                                   onClick={clickAwayHandler}>Discard</Button>
+                                       </InputAdornment> : null,
+                               }}/>
+                </Box>
+            </ClickAwayListener>
             <List>
                 <TransitionGroup>{notes.map((note, index) =>
-                    <Collapse key={note.title}><Grid item xs={6}>
-                        <Note title={note.title} date={note.date} isFavorited={note.isFavorited}
-                              details={note.details} changeNoteFunc={(actionType) => editNotes(index, actionType)}/>
-                    </Grid></Collapse>
+                    <Collapse key={note.title}>
+                        <Grid item xs={6}>
+                            <Note title={note.title} date={note.date} isFavorited={note.isFavorited}
+                                  details={note.details} changeNoteFunc={(actionType) => editNotes(index, actionType)}/>
+                        </Grid>
+                    </Collapse>
                 )}
                 </TransitionGroup>
             </List>
@@ -68,7 +118,7 @@ const NotesContainer = () => {
                 </Alert>
             </Snackbar>
             {!notes.length &&
-                <Container sx={{textAlign:"center"}}>
+                <Container sx={{textAlign: "center"}}>
                     <SentimentDissatisfied fontSize={"large"} color={"disabled"}/>
                     <Typography variant="subtitle2" sx={{color: "text.disabled"}}>It's a bit empty in here... Why not
                         add a new note?</Typography>
--- src/constants/components/notes-container/index.js
+++ src/constants/components/notes-container/index.js
@@ -1,5 +1,6 @@
 export const actionTypes = {
     "REMOVE": 1,
     "TOGGLE_FAVORITE": 2,
-    "SHARE": 3
+    "SHARE": 3,
+    "ADD": 4
 }
